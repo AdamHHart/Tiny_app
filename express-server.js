b796d24771393914fc38d3192dc9390368f076d7
Adam Hart <adamhayhart@gmail.com>
2021-03-15 13:34:39 -0700

    connected html template with urls_index.ejs in views. now /urls shows templateVars urlDatabase object (tiny urls)

--- express-server.js
+++ express-server.js
@@ -17,26 +17,22 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+})
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
-app.get("/adam", (req, res) => {
-  res.send("What's up it's ya boi Adam!");
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
+// app.get("/adam", (req, res) => {
+//   res.send("What's up it's ya boi Adam!");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
